6b55dd3130831081db30856e973e6ad6f10f2349
mot3 <hopdrmot3@gmail.com>
2021-03-05 16:28:46 +0330

    Navigate to a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Startup Name Generator',
       home: RandomWords(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
